8bbc6c278eed9c5e7ffafc6471dfc28f3efcb529
Ruby Boyarski <rboyarski@infomedia.com.au>
2015-07-26 11:48:51 +0300

    Added a leeway of 0 to the "verify" method to tell it to perform an expiry check on the token.

--- nginx-jwt.lua
+++ nginx-jwt.lua
@@ -47,7 +47,7 @@ function M.auth(claim_specs,claims_as_headers, fallback_to_cookies)
             ngx.log(ngx.INFO, "Token: " .. token)
 
             -- require valid JWT
-            local jwt_obj = jwt:verify(secret, token)
+            local jwt_obj = jwt:verify(secret, token, 0)
             if jwt_obj.verified == false then
                 ngx.log(ngx.WARN, "Invalid token: ".. jwt_obj.reason)
                 ngx.exit(ngx.HTTP_UNAUTHORIZED)
